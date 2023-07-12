4dcce69d3726dd3e071eae16cf940830417366c2
Mehak A Maqsood <86131437+MehakMaqsood@users.noreply.github.com>
2023-07-12 17:41:44 -0400

    newchanges

--- app.py
+++ app.py
@@ -6,17 +6,32 @@ from flask import (Flask, redirect, render_template, request,
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+   #print('Request for index page received')
+   if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        resume = request.files['resume']
+        
+        # Save the resume file
+        resume.save(resume.filename)
+        
+        # Perform further processing with the form data and resume file here
+        
+        return "Form submitted successfully!"
+    
+#return render_template('index.html')
+   return render_template('intern.html')
+
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
+#@app.route('/hello', methods=['POST'])
+"""
 def hello():
    name = request.form.get('name')
 
@@ -26,6 +41,7 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
+       """
 
 
 if __name__ == '__main__':
